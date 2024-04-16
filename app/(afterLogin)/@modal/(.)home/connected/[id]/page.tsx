6b53fdfc9fd36acbdf66d741b7a264444bf4ce3d
Hellicat <jhccop@naver.com>
2024-04-17 00:45:07 +0900

    intercepting route for connectedCompany

---
+++ app/(afterLogin)/@modal/(.)home/connected/[id]/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+import ConnectedCompany from "@/app/(afterLogin)/_components/homeModals/connectCompany";
+import {useRouter} from "next/navigation";
+
+export default function ModalConnectedCompany() {
+  return <ConnectedCompany />;
+}
---
+++ app/(afterLogin)/home/connected/[id]/page.tsx
@@ -0,0 +1,5 @@
+import ConnectedCompany from "../../../_components/homeModals/connectCompany";
+
+export default function ConnectedCompanyPage() {
+  return <ConnectedCompany />;
+}
