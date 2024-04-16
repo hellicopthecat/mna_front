2c7293809af16ab6ff089490a3feb3b137d54bc1
Hellicat <jhccop@naver.com>
2024-04-17 00:27:17 +0900

    edit header Comp

--- components/body/header.tsx
+++ components/body/header.tsx
@@ -6,8 +6,8 @@ export default function HeaderComp() {
   const path = usePathname();
   const title = path.match("/") && "HOME";
   return (
-    <header className=" bg-slate-500 h-16 flex justify-center items-center">
-      <h1 className="text-white text-center ">{title}</h1>
+    <header className=" border-b-2 border-slate-400 h-16 flex justify-center items-center">
+      <h1 className="font-bold text-center ">{title}</h1>
     </header>
   );
 }
