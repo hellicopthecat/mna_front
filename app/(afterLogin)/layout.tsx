8dfabbb6aefa2b7dc487609bec9f67b024c91c34
Hellicat <jhccop@naver.com>
2024-04-17 00:34:43 +0900

    prepare modal

--- app/(afterLogin)/layout.tsx
+++ app/(afterLogin)/layout.tsx
@@ -3,13 +3,16 @@ import HeaderComp from "@/components/body/header";
 
 export default function afterLoginLayout({
   children,
+  modal,
 }: {
   children: React.ReactNode;
+  modal: React.ReactNode;
 }) {
   return (
-    <div className="flex flex-col h-dvh justify-between">
+    <div className="flex flex-col h-dvh justify-between relative">
       <HeaderComp />
       <div className="flex-1">{children}</div>
+      {modal}
       <FooterCont />
     </div>
   );
