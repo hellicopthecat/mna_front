f177e72fbb1dfe00b0e7481c2e7ba5c7b3cd457a
Hellicat <jhccop@naver.com>
2024-04-17 15:26:30 +0900

    rename components

--- app/(afterLogin)/home/_components/CompanyCard.tsx
+++ app/(afterLogin)/home/_components/CompanyCard.tsx
@@ -9,7 +9,7 @@ interface IOwnCompanyMainPage {
   connectedCompanyCount: number;
   connectingCompanyCount: number;
 }
-export default function OwnCompany({company}: {company: IOwnCompanyMainPage}) {
+export default function CompanyCard({company}: {company: IOwnCompanyMainPage}) {
   return (
     <div
       key={company?.id}
@@ -27,17 +27,21 @@ export default function OwnCompany({company}: {company: IOwnCompanyMainPage}) {
           </span>
         )}
       </div>
-      <div className="flex items-center justify-between">
+      <div className="flex flex-col gap-3 items-center justify-between">
         <div className="flex items-center gap-2">
           <div className="w-5 h-5 bg-blue-400 rounded-full" />
-          <h3 className="font-bold text-2xl">{company?.companyName}</h3>
+          <h3 className="font-bold">
+            {company?.companyName.length > 10
+              ? `${company.companyName.substring(0, 10)}...`
+              : company.companyName}
+          </h3>
         </div>
-        <div className="flex  gap-4 font-semibold text-sm">
+        <div className="flex  gap-4 font-semibold text-sm text-center">
           <Link href={`/home/connected/${company.id}`}>
-            Fallowed {company?.connectedCompanyCount}
+            거래처 {company?.connectedCompanyCount}
           </Link>
           <Link href={`/home/connecting/${company.id}`}>
-            Fallowing {company?.connectingCompanyCount}
+            발주자 {company?.connectingCompanyCount}
           </Link>
         </div>
       </div>
