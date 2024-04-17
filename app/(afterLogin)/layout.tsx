a377e2c062fb50eadac3ff8f5a9cee153287e1f3
Hellicat <jhccop@naver.com>
2024-04-17 15:24:12 +0900

    edit afterLogin groups main layout

--- app/(afterLogin)/layout.tsx
+++ app/(afterLogin)/layout.tsx
@@ -1,19 +1,24 @@
+"use client";
 import FooterCont from "@/components/body/footer";
 import HeaderComp from "@/components/body/header";
+import useUser from "@/hooks/useUser";
 
-export default function afterLoginLayout({
+export default function AfterLoginLayout({
   children,
   modal,
 }: {
   children: React.ReactNode;
   modal: React.ReactNode;
 }) {
-  return (
+  const {data, loading} = useUser();
+  return data ? (
     <div className="flex flex-col h-dvh justify-between relative">
       <HeaderComp />
       <div className="flex-1">{children}</div>
       {modal}
       <FooterCont />
     </div>
+  ) : (
+    <div>로그아웃되셨습니다.</div>
   );
 }
