188005c0dd3511b42b42d285072f4e41dde52d3a
Hellicat <jhccop@naver.com>
2024-04-17 15:29:59 +0900

    intercepting routing c2c

--- app/(afterLogin)/@modal/(.)home/connected/[id]/page.tsx
+++ app/(afterLogin)/@modal/(.)home/connected/[id]/page.tsx
@@ -1,6 +1,4 @@
-"use client";
 import ConnectedCompany from "@/app/(afterLogin)/_components/homeModals/connectCompany";
-import {useRouter} from "next/navigation";
 
 export default function ModalConnectedCompany() {
   return <ConnectedCompany />;
---
+++ app/(afterLogin)/@modal/(.)home/connecting/[id]/page.tsx
@@ -0,0 +1,5 @@
+import ConnectingCompany from "@/app/(afterLogin)/_components/homeModals/connectingCompany";
+
+export default function ModalConnectingCompany() {
+  return <ConnectingCompany />;
+}
